8fb42e0376d8197a37d8c3b69c3c50e5fdcffda8
anj107 <119646066+anj107@users.noreply.github.com>
2025-07-03 00:43:13 +0800

    root

---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function showLoading(button, url = null) {
+    const spinner = button.querySelector('.spinner-border');
+    const text = button.querySelector('.button-text');
+
+    spinner?.classList.remove('d-none');
+    if (text) text.style.display = 'none';
+    button.disabled = true;
+
+    if (url) {
+        setTimeout(() => {
+            window.location.href = url;
+        }, 500);
+    }
+    // else: let the form submit normally
+}
